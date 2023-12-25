e636d60d2ec9a6db88a78238c835a4b6fd4605f2
Abdellah <you@example.com>
2023-12-25 21:05:49 +0100

    Admin-page still in progress (just front end and not fully done)

---
+++ admin.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/admin.css">
+</head>
+
+<body>
+
+    <?php
+    include 'header.php';
+    //check if user is admin or redirect; mazal
+    ?>
+
+    <div class="container">
+        <div class="container-title">
+            Les Hotels
+        </div>
+        <div class="value-edit">
+            <div class="value-name">hotel exemplaire</div>
+            <div class="edit">
+                <svg></svg>
+                <svg></svg>
+            </div>
+        </div>
+        <div class="value-edit">
+            <div class="value-name">hotel exemplaire</div>
+            <div class="edit">
+                <svg></svg>
+                <svg></svg>
+            </div>
+        </div>
+        <div class="value-edit">
+            <div class="value-name">hotel exemplaire</div>
+            <div class="edit">
+                <svg></svg>
+                <svg></svg>
+            </div>
+        </div>
+        <div class="value-edit">
+            <div class="value-name">hotel exemplaire</div>
+            <div class="edit">
+                <svg></svg>
+                <svg></svg>
+            </div>
+        </div>
+        <div class="value-edit">
+            <div class="value-name">hotel exemplaire</div>
+            <div class="edit">
+                <svg></svg>
+                <svg></svg>
+            </div>
+        </div>
+        <div class="value-edit">
+            <div class="value-name">hotel exemplaire</div>
+            <div class="edit">
+                <svg></svg>
+                <svg></svg>
+            </div>
+        </div>
+        <div class="value-edit">
+            <div class="value-name">hotel exemplaire</div>
+            <div class="edit">
+                <svg></svg>
+                <svg></svg>
+            </div>
+        </div>
+        <div class="page">
+            <div class="previus-page"></div>
+            <div class="page-number">2-7</div>
+            <div class="next-page"></div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="container-title">
+            Les Voyages
+        </div>
+        <div class="value-edit">
+            <div class="value-name">Voyage exemplaire</div>
+            <div class="edit">
+                <svg></svg>
+                <svg></svg>
+            </div>
+        </div>
+        <div class="value-edit">
+            <div class="value-name">Voyage exemplaire</div>
+            <div class="edit">
+                <svg></svg>
+                <svg></svg>
+            </div>
+        </div>
+        <div class="value-edit">
+            <div class="value-name">Voyage exemplaire</div>
+            <div class="edit">
+                <svg></svg>
+                <svg></svg>
+            </div>
+        </div>
+        <div class="value-edit">
+            <div class="value-name">Voyage exemplaire</div>
+            <div class="edit">
+                <svg></svg>
+                <svg></svg>
+            </div>
+        </div>
+        <div class="value-edit">
+            <div class="value-name">Voyage exemplaire</div>
+            <div class="edit">
+                <svg></svg>
+                <svg></svg>
+            </div>
+        </div>
+        <div class="value-edit">
+            <div class="value-name">Voyage exemplaire</div>
+            <div class="edit">
+                <svg></svg>
+                <svg></svg>
+            </div>
+        </div>
+        <div class="value-edit">
+            <div class="value-name">Voyage exemplaire</div>
+            <div class="edit">
+                <svg></svg>
+                <svg></svg>
+            </div>
+        </div>
+        <div class="page">
+            <div class="previus-page"></div>
+            <div class="page-number">1-3</div>
+            <div class="next-page"></div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="container-title">
+            Les Clients
+        </div>
+        <div class="value-edit">
+            <div class="value-name">Client exemplaire</div>
+            <div class="edit">
+                <svg></svg>
+                <svg></svg>
+            </div>
+        </div>
+        <div class="value-edit">
+            <div class="value-name">Client exemplaire</div>
+            <div class="edit">
+                <svg></svg>
+                <svg></svg>
+            </div>
+        </div>
+        <div class="value-edit">
+            <div class="value-name">Client exemplaire</div>
+            <div class="edit">
+                <svg></svg>
+                <svg></svg>
+            </div>
+        </div>
+        <div class="value-edit">
+            <div class="value-name">Client exemplaire</div>
+            <div class="edit">
+                <svg></svg>
+                <svg></svg>
+            </div>
+        </div>
+        <div class="value-edit">
+            <div class="value-name">Client exemplaire</div>
+            <div class="edit">
+                <svg></svg>
+                <svg></svg>
+            </div>
+        </div>
+        <div class="value-edit">
+            <div class="value-name">Client exemplaire</div>
+            <div class="edit">
+                <svg></svg>
+                <svg></svg>
+            </div>
+        </div>
+        <div class="value-edit">
+            <div class="value-name">Client exemplaire</div>
+            <div class="edit">
+                <svg></svg>
+                <svg></svg>
+            </div>
+        </div>
+        <div class="page">
+            <div class="previus-page"></div>
+            <div class="page-number">7-23</div>
+            <div class="next-page"></div>
+        </div>
+    </div>
+
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,54 @@
+<link rel="stylesheet" href="assets/css/header.css">
+<header class="header">
+
+    <div class="left-side">
+        <div class="logo">
+            <img src="./assets/Images/logo-removebg-preview.png" alt="logo" class="logo_img">
+            <div class="logo_name">
+                <a href="index.html">Travel <div>Agent</div></a>
+            </div>
+        </div>
+        <div class="nav-links">
+            <a href="index.html">Main</a>
+            <a href="#">Hotels</a>
+            <a href="#">Destination</a>
+            <a href="./settings.html">Settings</a>
+        </div>
+
+    </div>
+
+    <div class="right-side">
+
+        <div class="account">
+            <a href="./register.html">Register</a>
+            <a href="./sign in.html">Sign in</a>
+        </div>
+
+
+        <div id="menu-icon">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="4" cy="4" r="2" fill="#fff" />
+                <circle cx="4" cy="12" r="2" fill="#fff" />
+                <circle cx="4" cy="20" r="2" fill="#fff" />
+
+                <rect x="8" y="2" width="16" height="4" rx="2" fill="#fff" />
+                <rect x="8" y="10" width="16" height="4" rx="2" fill="#fff" />
+                <rect x="8" y="18" width="16" height="4" rx="2" fill="#fff" />
+            </svg>
+        </div>
+    </div>
+    <div class="mobile-header">
+        <div class="mobile-nav-links">
+            <a href="https://www.messenger.com/" target="_blank">Main</a>
+            <a href="https://www.youtube.com/" target="_blank">Hotels</a>
+            <a onclick="scrollToSection('destination')">Destination</a>
+            <a href="./settings.html">Settings</a>
+        </div>
+        <div class="mobile-account">
+            <a href="./register.html">Register</a>
+            <a href="./sign in.html">Sign in</a>
+        </div>
+    </div>
+</header>
+
+<div id="space"></div>
