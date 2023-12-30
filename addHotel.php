31589b444068c293ec4d9ed60487604a99eeb0ba
Abdellah <you@example.com>
2023-12-30 16:55:50 +0100

    css

--- addHotel.php
+++ addHotel.php
@@ -5,28 +5,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="assets/css/addHotel.css">
 </head>
 
 <body>
 
-    <form action="addHotel.php" method="GET">
-        Id: <input type="number" name="id"><br>
-        Name: <input type="text" name="name"><br>
-        LocationID: <input type="number" name="locationId"><br>
-        Description: <input type="text" name="description"><br>
-        Price: <input type="text" name="price"><br>
-        Rating: <input type="text" name="rating"><br>
-        ImageURLs: <input type="text" name="imageURLs"><br>
-        <input type="submit"><br>
-    </form>
+    <div class="container">
+        <form action="addHotel.php" method="GET">
+            <?php
+            if (isset($_GET['id'])) {
+                $id = $_GET['id'];
+                echo "<div class='section-title'>Editing Hotel $id</div>";
+            } else {
+                echo 'Id: <input type="number" name="id"><br>';
+            }
+            ?>
+            Name: <input type="text" name="name"><br>
+            LocationID: <input type="number" name="locationId"><br>
+            Description: <input type="text" name="description"><br>
+            Price: <input type="text" name="price"><br>
+            Rating: <input type="text" name="rating"><br>
+            ImageURLs: <input type="text" name="imageURLs"><br>
+            <input type="submit"><br>
 
-    <?php
-    if (isset($_GET['id'])) {
-        $id = $_GET['id'];
-        echo "id = $id";
-    }
-    ?>
-
+        </form>
+    </div>
 </body>
 
 </html>
