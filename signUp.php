0b7f40ea225aa64d944a9ac9d8fff1dab97c4c14
Abdellah <you@example.com>
2023-12-31 19:35:45 +0100

    email check

--- signUp.php
+++ signUp.php
@@ -18,15 +18,18 @@
     $email = $_POST['email'];
     // check if email is already used:
     $sqlQuery = "SELECT Email FROM users WHERE Email='$email'";
-    mysqli_query($conn, $sqlQuery);
-
-    // insert new user:
-    $sqlQuery = "INSERT INTO users (Username, Password, Email) VALUES 
-    ('$username', '$password', '$email')";
-    if (mysqli_query($conn, $sqlQuery)) {
-        echo "votre compte est cree";
+    $result = mysqli_query($conn, $sqlQuery);
+    if ($result->num_rows > 0) {
+        // Email already exists
+        echo "Email already exists";
     } else {
-        echo "erreur";
+        $sqlQuery = "INSERT INTO users (Username, Password, Email) VALUES 
+                        ('$username', '$password', '$email')";
+        if (mysqli_query($conn, $sqlQuery)) {
+            echo "votre compte est cree";
+        } else {
+            echo "erreur";
+        }
     }
 
     // create cookie: (user id):
