b96bef7bbda855a8b001dc73d284dc54b2f49335
Abdellah <you@example.com>
2024-01-16 21:50:40 +0100

    push

--- assets/js/scroll_effect.js
+++ assets/js/scroll_effect.js
@@ -1,12 +1,15 @@
 // JavaScript for changing header background on scroll
 window.addEventListener('scroll', function () {
     const header = document.querySelector('.header');
+    const sahla = document.getElementById('SahlaIMG');
     const scrollPosition = window.scrollY;
 
     // Adjust this value based on when you want the background to change
     if (scrollPosition > 1) {
         header.classList.add('scrolled');
+        sahla.setAttribute('src', './assets/Images/NLblack.png');
     } else {
         header.classList.remove('scrolled');
+        sahla.setAttribute('src', './assets/Images/NL.png');
     }
 });
