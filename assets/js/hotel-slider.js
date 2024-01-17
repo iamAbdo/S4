aa348c50bd16395c0cd7f033736bfebaa6c41f73
snou <you@example.com>
2024-01-17 10:22:47 +0100

    HOTEL

--- assets/js/hotel-slider.js
+++ assets/js/hotel-slider.js
@@ -31,3 +31,39 @@ const setCounterAndSlide = (index) => {
     counter = index;
     slideImage();
 };
+
+document.addEventListener('DOMContentLoaded', function () {
+    let currentImageIndex = 0;
+    const images = document.querySelectorAll('.thumbnail');
+    const totalImages = images.length;
+  
+    function showImage(index) {
+      images.forEach(image => image.classList.remove('active'));
+      images[index].classList.add('active');
+      const largeImage = document.querySelector('.main-imgs img');
+      largeImage.src = images[index].style.backgroundImage.slice(5, -2);
+    }
+  
+    function nextImage() {
+      currentImageIndex = (currentImageIndex + 1) % totalImages;
+      showImage(currentImageIndex);
+    }
+  
+    function prevImage() {
+      currentImageIndex = (currentImageIndex - 1 + totalImages) % totalImages;
+      showImage(currentImageIndex);
+    }
+  
+    setInterval(nextImage, 4000);
+  
+    document.querySelector('.arrow.left').addEventListener('click', prevImage);
+    document.querySelector('.arrow.right').addEventListener('click', nextImage);
+  
+    images.forEach((thumbnail, index) => {
+      thumbnail.addEventListener('click', function () {
+        showImage(index);
+      });
+    });
+  
+    showImage(currentImageIndex);
+  });
