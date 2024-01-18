f7f23be0d72025964d8aa51268bec769cf46c397
Abdellah <you@example.com>
2024-01-18 19:19:00 +0100

    book

--- admin.php
+++ admin.php
@@ -105,12 +105,12 @@
         <div class="container-title">
             Les Locations
             <div class="open-container">
-                <svg onclick="OpenHotel()" id="HotelClosed" width="32px" height="32px" viewBox="0 0 24 24" fill="none">
+                <svg onclick="OpenLoc()" id="LocClosed" width="32px" height="32px" viewBox="0 0 24 24" fill="none">
                     <path
                         d="M4 19V6.2C4 5.0799 4 4.51984 4.21799 4.09202C4.40973 3.71569 4.71569 3.40973 5.09202 3.21799C5.51984 3 6.0799 3 7.2 3H16.8C17.9201 3 18.4802 3 18.908 3.21799C19.2843 3.40973 19.5903 3.71569 19.782 4.09202C20 4.51984 20 5.0799 20 6.2V17H6C4.89543 17 4 17.8954 4 19ZM4 19C4 20.1046 4.89543 21 6 21H20M9 7H15M9 11H15M19 17V21"
                         stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                 </svg>
-                <svg class="opener" onclick="CloseHotel()" id="HotelOpen" width="32px" height="32px" viewBox="0 0 24 24"
+                <svg class="opener" onclick="CloseLoc()" id="LocOpen" width="32px" height="32px" viewBox="0 0 24 24"
                     fill="none">
                     <path
                         d="M12 10.4V20M12 10.4C12 8.15979 12 7.03969 11.564 6.18404C11.1805 5.43139 10.5686 4.81947 9.81596 4.43597C8.96031 4 7.84021 4 5.6 4H4.6C4.03995 4 3.75992 4 3.54601 4.10899C3.35785 4.20487 3.20487 4.35785 3.10899 4.54601C3 4.75992 3 5.03995 3 5.6V16.4C3 16.9601 3 17.2401 3.10899 17.454C3.20487 17.6422 3.35785 17.7951 3.54601 17.891C3.75992 18 4.03995 18 4.6 18H7.54668C8.08687 18 8.35696 18 8.61814 18.0466C8.84995 18.0879 9.0761 18.1563 9.29191 18.2506C9.53504 18.3567 9.75977 18.5065 10.2092 18.8062L12 20M12 10.4C12 8.15979 12 7.03969 12.436 6.18404C12.8195 5.43139 13.4314 4.81947 14.184 4.43597C15.0397 4 16.1598 4 18.4 4H19.4C19.9601 4 20.2401 4 20.454 4.10899C20.6422 4.20487 20.7951 4.35785 20.891 4.54601C21 4.75992 21 5.03995 21 5.6V16.4C21 16.9601 21 17.2401 20.891 17.454C20.7951 17.6422 20.6422 17.7951 20.454 17.891C20.2401 18 19.9601 18 19.4 18H16.4533C15.9131 18 15.643 18 15.3819 18.0466C15.15 18.0879 14.9239 18.1563 14.7081 18.2506C14.465 18.3567 14.2402 18.5065 13.7908 18.8062L12 20"
@@ -118,23 +118,24 @@
                 </svg>
             </div>
         </div>
-        <?php
+        <div id="Loc-data" class="container-data">
+            <?php
 
-        $sqlQuery = "SELECT LocationID,Name FROM locations LIMIT 10";
+            $sqlQuery = "SELECT LocationID,Name FROM locations LIMIT 10";
 
-        $result = mysqli_query($conn, $sqlQuery);
+            $result = mysqli_query($conn, $sqlQuery);
 
 
-        if ($result) {
+            if ($result) {
 
-            //data
-            while ($row = mysqli_fetch_assoc($result)) {
-                $LocationName = $row['Name'];
-                $LocationID = $row['LocationID'];
+                //data
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $LocationName = $row['Name'];
+                    $LocationID = $row['LocationID'];
 
-                echo '<div class="value-edit">';
-                echo "<div class='value-name'>$LocationID - $LocationName</div>";
-                echo '<div class="edit">
+                    echo '<div class="value-edit">';
+                    echo "<div class='value-name'>$LocationID - $LocationName</div>";
+                    echo '<div class="edit">
                         <svg width="32px" height="32px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path
                                 d="M21.2799 6.40005L11.7399 15.94C10.7899 16.89 7.96987 17.33 7.33987 16.7C6.70987 16.07 7.13987 13.25 8.08987 12.3L17.6399 2.75002C17.8754 2.49308 18.1605 2.28654 18.4781 2.14284C18.7956 1.99914 19.139 1.92124 19.4875 1.9139C19.8359 1.90657 20.1823 1.96991 20.5056 2.10012C20.8289 2.23033 21.1225 2.42473 21.3686 2.67153C21.6147 2.91833 21.8083 3.21243 21.9376 3.53609C22.0669 3.85976 22.1294 4.20626 22.1211 4.55471C22.1128 4.90316 22.0339 5.24635 21.8894 5.5635C21.7448 5.88065 21.5375 6.16524 21.2799 6.40005V6.40005Z"
@@ -149,15 +150,16 @@
                                 stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                         </svg>
                     </div></div>';
-            }
-            // Free result set
-            mysqli_free_result($result);
+                }
+                // Free result set
+                mysqli_free_result($result);
 
-        } else {
-            // Handle the case where the query fails
-            echo "<h1>Il exist Pas de location dans la base</h1>";
-        }
-        ?>
+            } else {
+                // Handle the case where the query fails
+                echo "<h1>Il exist Pas de location dans la base</h1>";
+            }
+            ?>
+        </div>
     </div>
 
     <div class="container">
@@ -177,28 +179,29 @@
                 </svg>
             </div>
         </div>
-        <?php
+        <div id="User-data" class="container-data">
+            <?php
 
-        $sqlQuery = "SELECT UserID,Username,Role FROM users ORDER BY Role DESC LIMIT 10";
+            $sqlQuery = "SELECT UserID,Username,Role FROM users ORDER BY Role DESC LIMIT 10";
 
-        $result = mysqli_query($conn, $sqlQuery);
+            $result = mysqli_query($conn, $sqlQuery);
 
 
-        if ($result) {
+            if ($result) {
 
-            //data
-            while ($row = mysqli_fetch_assoc($result)) {
-                $UserName = $row['Username'];
-                $UserID = $row['UserID'];
-                $Role = $row['Role'];
+                //data
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $UserName = $row['Username'];
+                    $UserID = $row['UserID'];
+                    $Role = $row['Role'];
 
-                echo '<div class="value-edit">';
-                echo "<div class='value-name'>$UserID - $UserName";
-                if ($Role == 'admin') {
-                    echo "($Role)";
-                }
-                echo "</div>";
-                echo '<div class="edit">
+                    echo '<div class="value-edit">';
+                    echo "<div class='value-name'>$UserID - $UserName";
+                    if ($Role == 'admin') {
+                        echo "($Role)";
+                    }
+                    echo "</div>";
+                    echo '<div class="edit">
                         <svg width="32px" height="32px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path
                                 d="M21.2799 6.40005L11.7399 15.94C10.7899 16.89 7.96987 17.33 7.33987 16.7C6.70987 16.07 7.13987 13.25 8.08987 12.3L17.6399 2.75002C17.8754 2.49308 18.1605 2.28654 18.4781 2.14284C18.7956 1.99914 19.139 1.92124 19.4875 1.9139C19.8359 1.90657 20.1823 1.96991 20.5056 2.10012C20.8289 2.23033 21.1225 2.42473 21.3686 2.67153C21.6147 2.91833 21.8083 3.21243 21.9376 3.53609C22.0669 3.85976 22.1294 4.20626 22.1211 4.55471C22.1128 4.90316 22.0339 5.24635 21.8894 5.5635C21.7448 5.88065 21.5375 6.16524 21.2799 6.40005V6.40005Z"
@@ -213,15 +216,16 @@
                                 stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                         </svg>
                     </div></div>';
-            }
-            // Free result set
-            mysqli_free_result($result);
+                }
+                // Free result set
+                mysqli_free_result($result);
 
-        } else {
-            // Handle the case where the query fails
-            echo "<h1>Il exist Pas de location dans la base</h1>";
-        }
-        ?>
+            } else {
+                // Handle the case where the query fails
+                echo "<h1>Il exist Pas de location dans la base</h1>";
+            }
+            ?>
+        </div>
     </div>
 
     <script>
@@ -240,6 +244,42 @@
             const OpenIcon = document.getElementById("HotelOpen");
 
             hotelsection.style.display = 'none';
+            ClosedIcon.style.display = 'block';
+            OpenIcon.style.display = 'none';
+        }
+        function OpenLoc() {
+            const Locsection = document.getElementById("Loc-data");
+            const ClosedIcon = document.getElementById("LocClosed");
+            const OpenIcon = document.getElementById("LocOpen");
+
+            Locsection.style.display = 'block';
+            ClosedIcon.style.display = 'none';
+            OpenIcon.style.display = 'block';
+        }
+        function CloseLoc() {
+            const Locsection = document.getElementById("Loc-data");
+            const ClosedIcon = document.getElementById("LocClosed");
+            const OpenIcon = document.getElementById("LocOpen");
+
+            Locsection.style.display = 'none';
+            ClosedIcon.style.display = 'block';
+            OpenIcon.style.display = 'none';
+        }
+        function OpenUser() {
+            const Usersection = document.getElementById("User-data");
+            const ClosedIcon = document.getElementById("UserClosed");
+            const OpenIcon = document.getElementById("UserOpen");
+
+            Usersection.style.display = 'block';
+            ClosedIcon.style.display = 'none';
+            OpenIcon.style.display = 'block';
+        }
+        function CloseUser() {
+            const Usersection = document.getElementById("User-data");
+            const ClosedIcon = document.getElementById("UserClosed");
+            const OpenIcon = document.getElementById("UserOpen");
+
+            Usersection.style.display = 'none';
             ClosedIcon.style.display = 'block';
             OpenIcon.style.display = 'none';
         }
