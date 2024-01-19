9ab262498b1a7a1f8753c1caf0c8923e8476319e
Abdellah <you@example.com>
2024-01-19 22:28:36 +0100

    hmed

--- hotels.php
+++ hotels.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" href="assets/css/include/header.css">
-    <link rel="stylesheet" href="assets/css/shop.css">
+    <link rel="stylesheet" href="assets/css/bab.css">
     <title>Document</title>
 </head>
 
